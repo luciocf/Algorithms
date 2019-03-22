22a6cb5845d8617b7e7da58ef33c3588f02beda8
Lúcio Cardoso <lucio.code@gmail.com>
2019-03-22 17:15:22 -0300

    Updated bridges and articulation points

--- Graphs/bridges_articulation.cpp
+++ Graphs/bridges_articulation.cpp
@@ -5,34 +5,34 @@
 
 using namespace std;
 
-const int MAXN = 1e5+10;
+const int maxn = 1e5+10;
 
-int in[MAXN], low[MAXN], t;
-bool art_point[MAXN];
-vector<int> grafo[MAXN], bridge;
+int in[maxn], low[maxn], tt;
 
-void DFS(int u, int p)
+bool art_point[maxn];
+
+vector<int> grafo[maxn], bridge;
+
+// One special case: When the root of the DFS tree has only one forward 
+// edge, it is not an articulation point
+
+void dfs(int u, int p)
 {
-	in[u] = low[u] = ++t;
+	in[u] = low[u] = ++tt;
 
 	for (auto v: grafo[u])
 	{
-		if (v == p) continue;
-
 		if (in[v] != 0)
 		{
-			low[u] = min(low[u], in[v]);
+			if (v != p) low[u] = min(low[u], in[v]);
 			continue;
 		}
 
-		DFS(v, u);
+		dfs(v, u);
 
 		if (low[v] > in[u]) bridge.push_back({u, v});
-		if (low[v] >= in[u]) art_point[u] = 1;
+		if (low[v] >= in[u]) art_point[u] = true;
 
 		low[u] = min(low[u], low[v]);
 	}
 }
-
-// One special case: When the root of the DFS tree has only one forward 
-// edge, it is not an articulation point
