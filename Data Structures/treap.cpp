21bcc05021424c3ca442e8600850b1e750d0b25d
Lúcio Cardoso <lucio.code@gmail.com>
2019-03-27 09:05:25 -0300

    Updated treap

--- Data Structures/treap.cpp
+++ Data Structures/treap.cpp
@@ -1,4 +1,5 @@
 // Treap
+// O(log n) depth
 
 #include <bits/stdc++.h>
 
@@ -6,69 +7,65 @@ using namespace std;
 
 struct node
 {
-	int v, w, sz;
-	node *l, *r;
+    int v, w, sz;
+    node *l, *r;
 
-	node()
-	{
-		v = 0, sz = 1, w = rand();
-		l = r = NULL;
-	}
+    node()
+    {
+        v = 0, sz = 1, w = rand();
+        l = r = NULL;
+    }
 };
 
 typedef node*& node_t;
 
-int sz(node *t)
-{
-	if (!t) return 0;
-	return t->sz;
-}
+int sz(node *t){return (t ? t->sz : 0);}
 
-void upd_size(node *t)
+void op(node *t)
 {
-	if (t) t->sz = sz(t->l)+sz(t->r)+1;
+    if (t) t->sz = sz(t->l)+sz(t->r)+1;
 }
 
 bool find(node *t, int x)
 {
-	if (!t) return false;
+    if (!t) return false;
 
-	if (t->v == x) return true;
-	else if (x > t->v) return find(t->r, x);
-	return find(t->l, x);
+    if (t->v == x) return true;
+    return find((x > t->v ? t->r : t->l), x);
 }
 
-void split(node *t, int x, node_t l, node_t r)
+void split(node *t, node_t l, node_t r, int v)
 {
-	if (!t) l = r = NULL;
-	else if (t->v > x) split(t->l, x, l, t->l), r = t;
-	else split(t->r, x, t->r, r), l = t;
+    if (!t) return void(l=r=NULL);
 
-	upd_size(t);
+    if (t->v > v) split(t->l, l, t->l, v), r = t;
+    else split(t->r, t->r, r, v), l = t;
+
+    op(t);
 }
 
 void merge(node_t t, node *l, node *r)
 {
-	if (!l || !r) t = (l ? l : r);
-	else if (l->w > r->w) merge(l->r, l->r, r), t = l;
-	else merge(r->l, l, r->l), t = r;
+    if (!l || !r) t = (l ? l : r);
+    else if (l->w > r->w) merge(l->r, l->r, r), t = l;
+    else merge(r->l, l, r->l), t = r;
 
-	upd_size(t);
+    op(t);
 }
 
-void insert(node_t t, node *it)
+void insert(node_t t, node *aux)
 {
-	if (!t) t = it;
-	else if (it->w > t->w) split(t, it->v, it->l, it->r), t = it;
-	else insert((it->v > t->v ? t->r : t->l), it);
+    if (!t) t = aux;
+    else if (aux->w > t->w) split(t, aux->l, aux->r, aux->v), t = aux;
+    else insert((aux->v > t->v ? t->r : t->l), aux);
 
-	upd_size(t);
+    op(t);
 }
 
-void erase(node_t t, int x)
+void erase(node_t t, int v)
 {
-	if (t->v == x) merge(t, t->l, t->r);
-	else erase((x > t->v ? t->r : t->l), x);
+    if (t->v == v) merge(t, t->l, t->r);
+    else erase((v > t->v ? t->r : t->l), v);
 
-	upd_size(t);
+    op(t);
 }
