bcda9a9d4a57c7035c7a29046c554106899f8e0f
Lúcio Cardoso <lucio.code@gmail.com>
2019-05-22 17:45:05 -0300

    Added convex hull

--- Geometry/convex_hull.cpp
+++ Geometry/convex_hull.cpp
@@ -2,57 +2,74 @@
 // O(n log n)
 
 #include <bits/stdc++.h>
+
 #define x first
 #define y second
 
 using namespace std;
 
-const int MAXN = 1e5+10;
+const int maxn = 1e5+10;
 
-typedef pair<int, int> ponto;
+typedef pair<int, int> pt;
 
-vector<ponto> a;
+int n;
 
-int cross(ponto a, ponto b)
+pt a[maxn];
+
+vector<pt> env;
+
+int cross(pt a, pt b)
 {
-    return a.x*b.y-a.y*b.x;
+	return a.x*b.y - a.y*b.x;
 }
 
-bool ccw(ponto p, ponto q, ponto r)
+bool cw(pt a, pt b, pt c)
 {
-    q.x -= p.x, q.y -= p.y;
-    r.x -= p.x, r.y -= p.y;
-    return (cross(q, r) > 0);
+	b.x -= a.x, b.y -= a.y;
+	c.x -= a.x, c.y -= a.y;
+
+	return cross(b, c) < 0;
 }
 
-bool cw(ponto p, ponto q, ponto r)
+bool ccw(pt a, pt b, pt c)
 {
-    q.x -= p.x, q.y -= p.y;
-    r.x -= p.x, r.y -= p.y;
-    return (cross(q, r) < 0);
+	b.x -= a.x, b.y -= a.y;
+	c.x -= a.x, c.y -= a.y;
+
+	return cross(b, c) > 0;
 }
 
-int main(void)
+void convex_hull(void)
 {
-    sort(a.begin(), a.end());
+	sort(a+1, a+n+1);
 
-    ponto p1 = a[0], p2 = a.back();
-    vector<ponto> up, down;
-    up.push_back(p1), down.push_back(p1);
+	vector<pt> lower, upper;
 
-    for (int i = 1; i < a.size(); i++)
-    {
-        if (i == a.size()-1 || cw(p1, a[i], p2))
-        {
-            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
-                up.pop_back();
-            up.push_back(a[i]);
-        }
-        if (i == a.size()-1 || ccw(p1, a[i], p2))
-        {
-            while (down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
-                down.pop_back();
-            down.push_back(a[i]);
-        }
-    }
+	pt p1 = a[1], p2 = a[n];
+
+	lower.push_back(p1), upper.push_back(p1);
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (i == n || cw(p1, a[i], p2))
+		{
+			while (upper.size() >= 2 && !cw(upper[upper.size()-2], upper.back(), a[i]))
+				upper.pop_back();
+
+			upper.push_back(a[i]);
+		}
+
+		if (i == n || ccw(p1, a[i], p2))
+		{
+			while (lower.size() >= 2 && !ccw(lower[lower.size()-2], lower.back(), a[i]))
+				lower.pop_back();
+
+			lower.push_back(a[i]);
+		}
+	}
+
+	for (int i = 0; i < upper.size(); i++)
+		env.push_back(upper[i]);
+	for (int i = lower.size()-2; i >= 1; i--)
+		env.push_back(lower[i]);
 }
